3b1655bbf8c7213991e9310ddbdd60bf8a0d61a8
niteshnigam <niteshnigam10@gmail.com>
2019-08-26 19:55:38 +0530

    chain code

---
+++ network/bin/_launch.sh
@@ -0,0 +1,4 @@
+#PEER_MODE=net
+#Command=dev-init.sh -e 
+#Generated: Mon Aug 26 13:59:49 UTC 2019 
+docker-compose  -f ./compose/docker-compose.base.yaml      -f ./compose/docker-compose.explorer.yaml    up -d --remove-orphans
